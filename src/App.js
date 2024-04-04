6483363b959adcd2869cbb404db5c47d78ad09f9
Tainá Santana <taina.santana1305@gmail.com>
2024-04-04 14:24:20 -0300

    Aula 18: Estruturand o projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    <p>Costs</p>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <footer>Footer</footer>
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route path="/company"element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+    </Router>
   );
 }
 
