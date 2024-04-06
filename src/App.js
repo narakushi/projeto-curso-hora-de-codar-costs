f0251a18da080129b4b1300a1f9d37882fc50ee1
Tainá Santana <taina.santana1305@gmail.com>
2024-04-06 15:33:38 -0300

    criando container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './components/pages/Home';
-import Contact from './components/pages/Contact';
-import Company from './components/pages/Company';
-import NewProject from './components/pages/NewProject';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
 
 function App() {
   return (
@@ -13,14 +14,17 @@ function App() {
         <Link to="/company">Empresa</Link>
         <Link to="/newproject">Novo projeto</Link>
       </div>
-      <footer>Footer</footer>
-      <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route path="/company"element={<Company />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/newproject" element={<NewProject />} />
-      </Routes>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+    <footer>Footer</footer>
     </Router>
+    
   );
 }
 
